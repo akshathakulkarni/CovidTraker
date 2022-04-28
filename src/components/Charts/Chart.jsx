1afb66b0c86cd0d6597707a2d5324c159d4a4225
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-04-28 02:38:35 -0400

    Define the structure and display data in form of Line Chart

--- src/components/Charts/Chart.jsx
+++ src/components/Charts/Chart.jsx
@@ -1,8 +1,53 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { fetchDailyData } from '../../api';
+import { Line, Bar } from 'react-chartjs-2';
+import { Chart, registerables } from 'chart.js';
+
+import styles from './Chart.module.css';
+
+//register all components before use
+Chart.register(...registerables);
 
 const Charts = () => {
+  const [ dailyData, setDailyData ] = useState([]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setDailyData(await fetchDailyData());
+    };
+    console.log(dailyData);
+    fetchAPI();
+  }, []);
+
+  const lineChart = (
+    dailyData.length 
+    ? (
+      <Line 
+      data={{
+        // loop over dailyData and return date as labels
+        labels: dailyData.map(({ date }) => date),
+        datasets: [{
+          label: 'Infected',
+          data: dailyData.map(({ confirmed }) => confirmed),
+          borderColor: '#3333ff',
+          fill: true
+        }, {
+          label: 'Deaths',
+          data: dailyData.map(({ deaths }) => deaths),
+          borderColor: 'red',
+          backgroundColor: 'rgba(255, 0, 0, 0, 0.5)',
+          fill: true
+        }]
+      }}
+    />
+    ) : null
+
+  );
+
   return (
-    <h1>Charts</h1>
+    <div className={styles.container}>
+      {lineChart}
+    </div>
   );
 };
 
