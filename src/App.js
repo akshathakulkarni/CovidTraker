5e659713444342fde37235357245338440700bab
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-04-28 00:27:24 -0400

    Fetch the recieved data, update the state and pass down the data to Cards component

--- src/App.js
+++ src/App.js
@@ -2,18 +2,29 @@ import React from 'react';
 
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
+import { fetchData } from './api';
 
 class App extends React.Component {
-  return() {
+  //define state object to pass down data to other components
+  state = {
+    data: {}, //initial state
+  }
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    console.log(fetchedData);
+    this.setState({data: fetchedData});
+  }
+  render() {
+    const { data } = this.state;
     return (
       <div className={styles.container}>
-        <Cards />
+        <Cards data={data}/>
         <CountryPicker />
         <Chart />
         
       </div>
     );
   } 
-};
+}
 
 export default App;
