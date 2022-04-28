ccdfb4060b65922fef02833b9d31773ab6691016
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-04-28 01:14:56 -0400

    Destructure data from props and display each value in Card format

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -9,7 +9,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate }}) => {
   }
   return (
     <div className={styles.container}>
-      <Grid container spacing={3} justify='center'>
+      <Grid container spacing={5} justify='center'>
         <Grid item component={Card}>
           <CardContent>
             <Typography color='textSecondary' gutterBottom>Infected</Typography>
