2a68c427da541d00ea4c92274b0391e3d993fd06
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-04-29 01:16:20 -0400

    Define the structure for Bar chart using the data obtained from props

--- src/components/Charts/Chart.jsx
+++ src/components/Charts/Chart.jsx
@@ -8,11 +8,12 @@ import styles from './Chart.module.css';
 //register all components before use
 Chart.register(...registerables);
 
-const Charts = () => {
+const Charts = ({data: { confirmed, recovered, deaths }, country }) => {
   const [ dailyData, setDailyData ] = useState([]);
 
   useEffect(() => {
     const fetchAPI = async () => {
+      //this data is used only for global data and represented using Line Chart
       setDailyData(await fetchDailyData());
     };
     console.log(dailyData);
@@ -42,6 +43,26 @@ const Charts = () => {
     />
     ) : null
 
+  );
+
+  const barChart = (
+    confirmed 
+    ? (
+      <Bar 
+      data={{
+        labels: ['Infected', 'Recovered', 'Deaths'],
+        datasets: [{
+          label: 'People',
+          backgroundColor: ['rgba(0, 0, 255, 0.5)', 'rgba(0, 255, 0, 0.5)', 'rgba(255, 0, 0, 0.5)']
+        }],
+        data: [ confirmed, recovered, deaths]
+      }}
+      options={{
+        legend: {display: false},
+        title: { display: true, text: `Current state in ${country}`}
+      }}
+      />
+    ) : null
   );
 
   return (
