f248770abe153a5fce134b7e6ebc26cccac6eba1
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-04-29 02:17:32 -0400

    Import and render an image as the title

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
 import { fetchData } from './api';
+import coronaImage from './images/covid-19.png';
 
 class App extends React.Component {
   //define state object to pass down data to other components
@@ -31,7 +32,7 @@ class App extends React.Component {
     const { data, country } = this.state;
     return (
       <div className={styles.container}>
-        <h1>Covid Traker</h1>
+        <img className={styles.image} src={coronaImage} alt='Covid-19' />
         <Cards data={data}/>
         <CountryPicker handleCountryChange={this.handleCountryChange}/>
         <Chart data={data} country={country}/>
