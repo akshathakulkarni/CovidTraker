201ea3ea6e5c1258efc92f9900446cb7407f1198
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-04-29 00:57:43 -0400

    Add a handleCountryChange function to set the state with data based on country's name and pass it down as a prop to CountryPicker component

--- src/App.js
+++ src/App.js
@@ -8,19 +8,32 @@ class App extends React.Component {
   //define state object to pass down data to other components
   state = {
     data: {}, //initial state
+    country: ''
   }
   async componentDidMount() {
+    //fetch the data from api
     const fetchedData = await fetchData();
-    console.log(fetchedData);
+    //set the state with fetchedData setState({data: fetchedData})
     this.setState({data: fetchedData});
   }
+
+  handleCountryChange = async (country) => {
+    console.log(country);
+    //fetch the country
+    const fetchedcountryData = await fetchData(country);
+    console.log(fetchedcountryData);
+    //set the state with received country name
+    this.setState({ data: fetchedcountryData, country: country });
+
+  }
+
   render() {
     const { data } = this.state;
     return (
       <div className={styles.container}>
         <h1>Covid Traker</h1>
         <Cards data={data}/>
-        <CountryPicker />
+        <CountryPicker handleCountryChange={this.handleCountryChange}/>
         <Chart />
         
       </div>
